d25cadfb36731a06c9aaf1bd46d955a58d0d3a34
luuca792 <luuca792@gmail.com>
2023-04-05 23:42:19 +0700

    Ajax requests is now use Promises. List of product types in homepage and design page is now sorted in alphabetical order. Navigation bar fix. Add draft product selection view pop-op when click on the plus icon in desgin view

--- src/JS/design.js
+++ src/JS/design.js
@@ -1,118 +1,159 @@
 // Item dynamic price
 $(function () {
-    loadAllProducts();
-  
-    const originalPrices = document.querySelectorAll(".item_original_price");
-    const discountPrices = document.querySelectorAll(".item_discount_price");
-    console.log(originalPrices);
-  
-    for (let i = 0; i < originalPrices.length; i++) {
-      if (discountPrices[i].innerText !== "0đ") {
-        // console.log('Discount price is not zero:', discountPrices[i].innerText);
-        originalPrices[i].classList.add("discount");
-        discountPrices[i].classList.add("has_discount_price");
-      } else {
-        // console.log('Discount price is zero:', discountPrices[i].innerText);
-      }
-    }
-  });
+  loadAllProducts(afterLoadAllProducts);
+});
 
-// AJAX
+function loadAllProducts(callback) {
+  $("#product-result").html("");
 
-function loadAllProducts() {
-$("#product-result").html("");
-
-// Make a ajax request call
-var request = $.ajax({
+  // Make a ajax request call
+  var request = $.ajax({
     headers: {
-    Accept: "application/json",
-    "Content-Type": "application/jsons",
+      Accept: "application/json",
+      "Content-Type": "application/jsons",
     },
     url: "/product-types",
     method: "GET",
-});
+  });
 
-request.done((data) => {
-    data.forEach((element) => {
-    var out = "";
-    out += "<div class='item_set_container'>";
-    out += "<h3 class='set_label'>" + element.productTypeName + "</h3>" + "<div class='set_item'>";
+  request.done((data) => {
+    // Sort the data by productTypeName
+    data.sort((a, b) => a.productTypeName.localeCompare(b.productTypeName));
 
-    out += "<div class='new_item_ctn'>";
-    out += "<div class='new-item'>"
-    out += "<img src='/images/materials/plus.png' alt='' />";
-    out += "</div> </div>";
+    const promises = [];
 
-    // Call to get products by type api
-    var encodedProductTypeName = encodeURIComponent(element.productTypeName);
+    // Loop through the sorted data and add each $.ajax call to promises array
+    for (let i = 0; i < data.length; i++) {
+      const element = data[i];
+      const encodedProductTypeName = encodeURIComponent(element.productTypeName);
 
-    $.ajax({
-        headers: {
-        Accept: "application/json",
-        "Content-Type": "application/jsons",
-        },
-        url: "/products?type=" + encodedProductTypeName,
-        method: "GET",
-        async: false, // set async to false
-        success: function (product_data) {
-        product_data.forEach((product_element) => {
-            var product_out = "";
+      const promise = new Promise((resolve, reject) => {
+        var out = "";
+        out += "<div class='item_set_container'>";
+        out += "<h3 class='set_label'>" + element.productTypeName + "</h3>" + "<div class='set_item'>";
 
-            //Format product price to have dot according to VND
-            const formattedproductPrice = product_element.productPrice.toLocaleString("vi-VN");
+        out += "<div class='new-item-ctn'>";
+        out += "<div class='new-item'>";
+        out += "<img src='/images/materials/plus.png' alt='' />";
+        out += "</div> </div>";
 
-            product_out +=
-            "<div class='item_ctn'><a href='product.html' style='text-decoration: none'>" +
-            "<div class='item'>" +
-            "<img src='/images/god_of_war_rng.jpg' alt='' />" +
-            "</div>" +
-            "<div class='item_name'>" +
-            "<h3>" +
-            product_element.productName +
-            "</h3>" +
-            "</div>" +
-            "<div class='item_price'>" +
-            "<h3>" +
-            formattedproductPrice +
-            "đ</h3>" +
-            "</div>" +
-            "</a></div>";
+        // Call to get products by type api
+        $.ajax({
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/jsons",
+          },
+          url: "/products?type=" + encodedProductTypeName,
+          method: "GET",
+          success: function (product_data) {
+            product_data.forEach((product_element) => {
+              var product_out = "";
 
-            out += product_out;
+              //Format product price to have dot according to VND
+              const formattedproductPrice = product_element.productPrice.toLocaleString("vi-VN");
+
+              product_out +=
+                "<div class='item_ctn'><a href='product.html' style='text-decoration: none'>" +
+                "<div class='item'>" +
+                "<img src='/images/god_of_war_rng.jpg' alt='' />" +
+                "</div>" +
+                "<div class='item_name'>" +
+                "<h3>" +
+                product_element.productName +
+                "</h3>" +
+                "</div>" +
+                "<div class='item_price'>" +
+                "<h3 class='item_original_price'>" +
+                formattedproductPrice +
+                "đ</h3>" +
+                "<h3 class='item_discount_price'>150.000đ</h3>" +
+                "</div>" +
+                "</a></div>";
+
+              out += product_out;
+            });
+
+            // Close set-item
+            out += "</div>";
+
+            // Close item-set-container
+            out += "</div>";
+
+            resolve(out);
+          },
+          error: function (jqXHR, textStatus, errorThrown) {
+            console.log("Error: " + textStatus + " - " + errorThrown);
+            reject(errorThrown);
+          },
         });
-        },
-        error: function (jqXHR, textStatus, errorThrown) {
-        console.log("Error: " + textStatus + " - " + errorThrown);
-        },
-    });
+      });
 
-    // Close set-item
-    out += "</div>";
-    // Close item-set-container
-    out += "</div>";
+      promises.push(promise);
+    }
 
-    $("#product-result").append(out);
-    });
-});
+    // Use Promise.all() to ensure that the promises are executed in the correct order
+    Promise.all(promises)
+      .then((results) => {
+        results.forEach((result) => {
+          $("#product-result").append(result);
+        });
+        callback();
+      })
+      .catch((error) => {
+        console.log("Error: " + error);
+      });
+  });
 }
-  
+
 function getAllProducts() {
-var request = $.ajax({
+  var request = $.ajax({
     headers: {
-    Accept: "application/json",
-    "Content-Type": "application/jsons",
+      Accept: "application/json",
+      "Content-Type": "application/jsons",
     },
     url: "/products",
     method: "GET",
-});
-request.done((data) => {
+  });
+  request.done((data) => {
     console.log("Get products");
     data.forEach((element) => {
-    console.log(element);
+      console.log(element);
     });
-});
-request.fail((err) => {
+  });
+  request.fail((err) => {
     console.log(err);
-});
+  });
 }
-  
+
+function afterLoadAllProducts() {
+  console.log("After load all products called");
+
+  const originalPrices = document.querySelectorAll(".item_original_price");
+  const discountPrices = document.querySelectorAll(".item_discount_price");
+  console.log(originalPrices);
+
+  for (let i = 0; i < originalPrices.length; i++) {
+    if (discountPrices[i].innerText !== "0đ") {
+      originalPrices[i].classList.add("discount");
+      discountPrices[i].classList.add("has_discount_price");
+    }
+  }
+
+  const addProductButtons = document.querySelectorAll(".new-item");
+  const productSelection = document.querySelector(".product-selection-view");
+  const closeButton = productSelection.querySelector(".close-button");
+  const darkFilter = document.querySelector(".dark-filter");
+
+  addProductButtons.forEach((button) => {
+    console.log(button);
+    button.addEventListener("click", () => {
+      productSelection.style.display = "flex";
+      darkFilter.style.display = "block";
+    });
+  });
+
+  closeButton.addEventListener("click", () => {
+    productSelection.style.display = "none";
+    darkFilter.style.display = "none";
+  });
+}
--- src/JS/homepage.js
+++ src/JS/homepage.js
@@ -1,25 +1,9 @@
 // Item dynamic price
 $(function () {
-  loadAllProducts();
-
-  const originalPrices = document.querySelectorAll(".item_original_price");
-  const discountPrices = document.querySelectorAll(".item_discount_price");
-  console.log(originalPrices);
-
-  for (let i = 0; i < originalPrices.length; i++) {
-    if (discountPrices[i].innerText !== "0đ") {
-      // console.log('Discount price is not zero:', discountPrices[i].innerText);
-      originalPrices[i].classList.add("discount");
-      discountPrices[i].classList.add("has_discount_price");
-    } else {
-      // console.log('Discount price is zero:', discountPrices[i].innerText);
-    }
-  }
+  loadAllProducts(afterLoadAllProducts);
 });
 
-// AJAX
-
-function loadAllProducts() {
+function loadAllProducts(callback) {
   $("#product-result").html("");
 
   // Make a ajax request call
@@ -33,66 +17,88 @@ function loadAllProducts() {
   });
 
   request.done((data) => {
-    data.forEach((element) => {
-      var out = "";
-      out += "<div class='item_set_container'>";
-      out += "<h3 class='set_label'>" + element.productTypeName + "</h3>" + "<div class='set_item'>";
+    // Sort the data by productTypeName
+    data.sort((a, b) => a.productTypeName.localeCompare(b.productTypeName));
 
-      // Call to get products by type api
-      var encodedProductTypeName = encodeURIComponent(element.productTypeName);
+    const promises = [];
 
-      $.ajax({
-        headers: {
-          Accept: "application/json",
-          "Content-Type": "application/jsons",
-        },
-        url: "/products?type=" + encodedProductTypeName,
-        method: "GET",
-        async: false, // set async to false
-        success: function (product_data) {
-          product_data.forEach((product_element) => {
-            var product_out = "";
+    // Loop through the sorted data and add each $.ajax call to promises array
+    for (let i = 0; i < data.length; i++) {
+      const element = data[i];
+      const encodedProductTypeName = encodeURIComponent(element.productTypeName);
 
-            //Format product price to have dot according to VND
-            const formattedproductPrice = product_element.productPrice.toLocaleString("vi-VN");
+      const promise = new Promise((resolve, reject) => {
+        var out = "";
+        out += "<div class='item_set_container'>";
+        out += "<h3 class='set_label'>" + element.productTypeName + "</h3>" + "<div class='set_item'>";
 
-            product_out +=
-              "<div class='item_ctn'><a href='product.html' style='text-decoration: none'>" +
-              "<div class='item'>" +
-              "<img src='/images/god_of_war_rng.jpg' alt='' />" +
-              "</div>" +
-              "<div class='item_name'>" +
-              "<h3>" +
-              product_element.productName +
-              "</h3>" +
-              "</div>" +
-              "<div class='item_price'>" +
-              "<h3>" +
-              formattedproductPrice +
-              "đ</h3>" +
-              "</div>" +
-              "</a></div>";
+        // Call to get products by type api
+        $.ajax({
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/jsons",
+          },
+          url: "/products?type=" + encodedProductTypeName,
+          method: "GET",
+          success: function (product_data) {
+            product_data.forEach((product_element) => {
+              var product_out = "";
 
-            out += product_out;
-          });
-        },
-        error: function (jqXHR, textStatus, errorThrown) {
-          console.log("Error: " + textStatus + " - " + errorThrown);
-        },
+              //Format product price to have dot according to VND
+              const formattedproductPrice = product_element.productPrice.toLocaleString("vi-VN");
+
+              product_out +=
+                "<div class='item_ctn'><a href='product.html' style='text-decoration: none'>" +
+                "<div class='item'>" +
+                "<img src='/images/god_of_war_rng.jpg' alt='' />" +
+                "</div>" +
+                "<div class='item_name'>" +
+                "<h3>" +
+                product_element.productName +
+                "</h3>" +
+                "</div>" +
+                "<div class='item_price'>" +
+                "<h3 class='item_original_price'>" +
+                formattedproductPrice +
+                "đ</h3>" +
+                "<h3 class='item_discount_price'>150.000đ</h3>" +
+                "</div>" +
+                "</a></div>";
+
+              out += product_out;
+            });
+
+            // Close set-item
+            out += "</div>";
+
+            // Close item-set-container
+            out += "</div>";
+
+            resolve(out);
+          },
+          error: function (jqXHR, textStatus, errorThrown) {
+            console.log("Error: " + textStatus + " - " + errorThrown);
+            reject(errorThrown);
+          },
+        });
       });
 
-      // Close set-item
-      out += "</div>";
-      // Close item-set-container
-      out += "</div>";
+      promises.push(promise);
+    }
 
-      $("#product-result").append(out);
-    });
+    // Use Promise.all() to ensure that the promises are executed in the correct order
+    Promise.all(promises)
+      .then((results) => {
+        results.forEach((result) => {
+          $("#product-result").append(result);
+        });
+        callback();
+      })
+      .catch((error) => {
+        console.log("Error: " + error);
+      });
   });
 }
-
-
-
 
 function getAllProducts() {
   var request = $.ajax({
@@ -112,4 +118,17 @@ function getAllProducts() {
   request.fail((err) => {
     console.log(err);
   });
+}
+
+function afterLoadAllProducts() {
+  const originalPrices = document.querySelectorAll(".item_original_price");
+  const discountPrices = document.querySelectorAll(".item_discount_price");
+  console.log(originalPrices);
+
+  for (let i = 0; i < originalPrices.length; i++) {
+    if (discountPrices[i].innerText !== "0đ") {
+      originalPrices[i].classList.add("discount");
+      discountPrices[i].classList.add("has_discount_price");
+    }
+  }
 }
