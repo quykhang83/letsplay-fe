a800703b8d31683abc934d5e1277076127c5e8c5
luuca792 <luuca792@gmail.com>
2023-04-22 14:28:18 +0700

    fisrt step at dynamic library

--- src/JS/library.js
+++ src/JS/library.js
@@ -1,23 +1,49 @@
-
-// keycloak
-//   .init({
-//     onLoad: 'login-required',
-//     checkLoginIframe: false,
-//     promiseType: 'native',
-//   })
-//   .then(() => {
-//     // localStorage.setItem('isLoggedIn', true);
-//   })
-//   .catch((error) => {
-//     alert('Something went wrong due to \n' + error);
-//   });
 import { keycloak, authenticateLogin } from './keycloakauth.js';
 $(async function () {
   await authenticateLogin();
   if (keycloak.authenticated) {
-    console.log("User is authenticated");
+    console.log('User is authenticated');
   } else {
-    console.log("User is not authenticated! Calling authenticate function");
-    console.log("Authentication status: ", keycloak.authenticated);
+    console.log('User is not authenticated! Calling authenticate function');
+    console.log('Authentication status: ', keycloak.authenticated);
   }
+  await initialized();
 });
+async function initialized() {
+  await loadGameList();
+}
+
+async function loadGameList() {
+  console.log("Loading game list");
+  $('#game-list-result').html('');
+
+  var product = $.ajax({
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+      Authorization: 'Bearer ' + keycloak.token,
+    },
+    url: '/users' + '/library',
+    method: 'GET',
+  });
+
+  product
+    .then((data) => {
+      console.log(data);
+      var out = '';
+      for (let i = 0; i < data.length; i++) {
+        out +=
+          "<div class='game-container'>" +
+          "<div class='game-inlist-img'></div>" +
+          "<div class='game-inlist-title'>" +
+          '<h3>' +
+          data[i].productName +
+          '</h3>' +
+          '</div> </div>';
+      }
+      $('#game-list-result').append(out);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}
